323843e3e97c963b4f3b8cc5ca089ab350fbeeed
Shvaib-Khan <svaibk123@gmail.com>
2026-07-19 19:13:04 +0530

    Configure MySQL and Redis connectivity; update dependencies and add connection logic

---
+++ src/config/redis.js
@@ -0,0 +1,10 @@
+import {createClient} from "redis"
+
+const redisClient = createClient({
+    socket:{
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+});
+
+export {redisClient}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import dotenv from "dotenv"
 import { app } from "./app.js";
+import { connectWithRetry } from "./db/index.js";
+import { redisConnect } from "./redis/index.js";
 
 dotenv.config({
     path: './.env'
 })
+
+
+async function startServer() {
+    try {
+        // Connect MySQL
+        await connectWithRetry()
        
-app.listen(process.env.PORT  || 3000, () => {
-    console.log(`Server is running at port: ${process.env.PORT}`)
-    console.log(`Serves at http://localhost:${process.env.PORT}`)
-});
+        // Connect Redis
+        await redisConnect()
+
+        // Start Express
+        app.listen(process.env.PORT  || 3000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`)
+            console.log(`Serves at http://localhost:${process.env.PORT}`)
+        });
+
+    } catch (error) {
+        console.error("Error while starting the server:", error);
+        process.exit(1);
+    }
+}
+
+
+startServer();
+
