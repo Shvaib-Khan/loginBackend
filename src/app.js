20dca73c13c3b9570626913d2b45d17c0da07a27
Shvaib-Khan <svaibk123@gmail.com>
2026-07-21 12:52:24 +0530

    Implement user registration feature with password hashing

--- src/app.js
+++ src/app.js
@@ -11,4 +11,19 @@ app.get("/", (req, res) => {
 app.use("/api/v1/users", userRouter);
 app.use(errorHandler);
 
+<<<<<<< HEAD
 export { app };
+=======
+app.use(express.json());
+
+
+import userRouter from './routes/user.route.js';
+app.use("/api/v1/users", userRouter)
+
+app.get('/',(req,res)=>{
+    res.send("This is home route")
+})
+
+
+export {app}
+>>>>>>> c9b6582 (Implement user registration feature with password hashing)
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
@@ -6,3 +7,13 @@ const router = Router();
 router.route("/register").post(registerUser);
 
 export default router;
+=======
+import {Router} from "express"
+import { registerUser } from "../controllers/user.controller.js"
+
+const router = Router()
+
+router.route('/register').post(registerUser)
+
+export default router;
+>>>>>>> c9b6582 (Implement user registration feature with password hashing)
