2af45b6aa9131c81ff7b48ee756e805f272b8279
Shvaib-Khan <svaibk123@gmail.com>
2026-07-22 22:28:04 +0530

    Enhance API reliability by implementing centralized error handling and response formatting

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { errorHandler } from "./middlewares/error.middleware.js"
 import cors from "cors"
 
 const app = express()
@@ -12,11 +13,15 @@ app.use(express.json());
 
 
 import userRouter from './routes/user.route.js';
+
 app.use("/api/v1/users", userRouter)
 
 app.get('/',(req,res)=>{
     res.send("This is home route")
 })
+
+//keep this middleware alwways in last
+app.use(errorHandler)
 
 
 export {app}
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,22 +1,61 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = async (req, res)=>{
+const registerUser = asyncHandler( async (req, res)=>{
 
-    try {
+    // try {
+    //     const {name, email, password} = req.body;
+    
+    //     if(!name || !email || !password){
+    //         return res
+    //         .status(400)
+    //         .json({message: "All fields are required"})
+    //     }
+    
+    //     const existingUser = await User.findByEmail(email);
+    //     if(existingUser){
+    //         return res
+    //         .status(409)
+    //         .json({message: "Email already exist"})
+    //     }
+    
+    //     const hashedPassword = await bcrypt.hash(password, 10)
+    
+    //     await User.create({
+    //       name,
+    //       email,
+    //       password: hashedPassword
+    //     });
+    
+    //     const createdUser = await User.findByEmail(email);
+    
+    //     if (!createdUser) {
+    //         throw new ApiError(500, "Something went wrong while registering the user")
+    //     }
+    
+    //     return res.status(201).json({message:"User registered successfully"}, createdUser);
+    // } catch (error) {
+    //     console.error(error)
+    //     res
+    //     .status(500)
+    //     .json({message: "Internal server error"});
+    // }
+
+    //--> instead of wrapping whole code in try catch block use asyncHandler in each controller
+    //--> use apiResnponse and apiError standard format
+
         const {name, email, password} = req.body;
     
         if(!name || !email || !password){
-            return res
-            .status(400)
-            .json({message: "All fields are required"})
+            throw new ApiError(400, "All fields are required")
         }
     
         const existingUser = await User.findByEmail(email);
         if(existingUser){
-            return res
-            .status(409)
-            .json({message: "Email already exist"})
+            throw new ApiError(409, "Email already exist")
         }
     
         const hashedPassword = await bcrypt.hash(password, 10)
@@ -33,14 +72,13 @@ const registerUser = async (req, res)=>{
             throw new ApiError(500, "Something went wrong while registering the user")
         }
     
-        return res.status(201).json({message:"User registered successfully"}, createdUser);
-    } catch (error) {
-        console.error(error)
-        res
-        .status(500)
-        .json({message: "Internal server error"});
-    }
+        return res
+        .status(201)
+        .json(
+            new ApiResponse(200, createdUser, "User registered successfully")
+        );
 
-}
+
+});
 
 export {registerUser}
